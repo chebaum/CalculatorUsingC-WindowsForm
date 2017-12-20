79a850e08da4ff4e83fa27324346d425a8aace18
chebaum <chebaum@naver.com>
2017-12-20 14:06:27 +0900

    필요없는 주석 제거 및 설명용 주석 추가 키보드로도 input받을 수 있도록 keydown event handler 추가함.

--- calcV2/CalculatorV1.Designer.cs
+++ calcV2/CalculatorV1.Designer.cs
@@ -36,7 +36,6 @@
             this.subBtn = new System.Windows.Forms.Button();
             this.backspaceBtn = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
             this.clrBtn = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -47,6 +46,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button0 = new System.Windows.Forms.Button();
             this.str = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // calcStr
@@ -63,17 +63,20 @@
             // 
             // enterBtn
             // 
+            this.enterBtn.CausesValidation = false;
             this.enterBtn.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.enterBtn.Location = new System.Drawing.Point(249, 366);
             this.enterBtn.Name = "enterBtn";
             this.enterBtn.Size = new System.Drawing.Size(88, 56);
-            this.enterBtn.TabIndex = 17;
+            this.enterBtn.TabIndex = 3;
             this.enterBtn.Text = "=";
             this.enterBtn.UseVisualStyleBackColor = true;
             this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
+            this.enterBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // divBtn
             // 
+            this.divBtn.CausesValidation = false;
             this.divBtn.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.divBtn.Location = new System.Drawing.Point(249, 304);
             this.divBtn.Name = "divBtn";
@@ -82,9 +85,11 @@
             this.divBtn.Text = "/";
             this.divBtn.UseVisualStyleBackColor = true;
             this.divBtn.Click += new System.EventHandler(this.operand_Click);
+            this.divBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // mulBtn
             // 
+            this.mulBtn.CausesValidation = false;
             this.mulBtn.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.mulBtn.Location = new System.Drawing.Point(249, 242);
             this.mulBtn.Name = "mulBtn";
@@ -93,9 +98,11 @@
             this.mulBtn.Text = "*";
             this.mulBtn.UseVisualStyleBackColor = true;
             this.mulBtn.Click += new System.EventHandler(this.operand_Click);
+            this.mulBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // sumBtn
             // 
+            this.sumBtn.CausesValidation = false;
             this.sumBtn.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.sumBtn.Location = new System.Drawing.Point(249, 118);
             this.sumBtn.Name = "sumBtn";
@@ -104,9 +111,11 @@
             this.sumBtn.Text = "+";
             this.sumBtn.UseVisualStyleBackColor = true;
             this.sumBtn.Click += new System.EventHandler(this.operand_Click);
+            this.sumBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // subBtn
             // 
+            this.subBtn.CausesValidation = false;
             this.subBtn.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.subBtn.Location = new System.Drawing.Point(249, 180);
             this.subBtn.Name = "subBtn";
@@ -115,9 +124,11 @@
             this.subBtn.Text = "-";
             this.subBtn.UseVisualStyleBackColor = true;
             this.subBtn.Click += new System.EventHandler(this.operand_Click);
+            this.subBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // backspaceBtn
             // 
+            this.backspaceBtn.CausesValidation = false;
             this.backspaceBtn.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.backspaceBtn.Location = new System.Drawing.Point(91, 366);
             this.backspaceBtn.Name = "backspaceBtn";
@@ -126,10 +137,12 @@
             this.backspaceBtn.Text = "backspace";
             this.backspaceBtn.UseVisualStyleBackColor = true;
             this.backspaceBtn.Click += new System.EventHandler(this.backspaceBtn_Click);
+            this.backspaceBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button9
             // 
             this.button9.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button9.CausesValidation = false;
             this.button9.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button9.Location = new System.Drawing.Point(170, 180);
             this.button9.Name = "button9";
@@ -138,21 +151,11 @@
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button0_Click);
-            // 
-            // button8
-            // 
-            this.button8.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button8.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button8.Location = new System.Drawing.Point(91, 180);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(73, 56);
-            this.button8.TabIndex = 2;
-            this.button8.Text = "8";
-            this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.button0_Click);
+            this.button9.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // clrBtn
             // 
+            this.clrBtn.CausesValidation = false;
             this.clrBtn.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.clrBtn.Location = new System.Drawing.Point(12, 118);
             this.clrBtn.Name = "clrBtn";
@@ -161,10 +164,12 @@
             this.clrBtn.Text = "CE";
             this.clrBtn.UseVisualStyleBackColor = true;
             this.clrBtn.Click += new System.EventHandler(this.clrBtn_Click);
+            this.clrBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button7
             // 
             this.button7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button7.CausesValidation = false;
             this.button7.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button7.Location = new System.Drawing.Point(12, 180);
             this.button7.Name = "button7";
@@ -173,10 +178,12 @@
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button0_Click);
+            this.button7.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button5.CausesValidation = false;
             this.button5.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button5.Location = new System.Drawing.Point(91, 242);
             this.button5.Name = "button5";
@@ -185,10 +192,12 @@
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button0_Click);
+            this.button5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button4.CausesValidation = false;
             this.button4.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button4.Location = new System.Drawing.Point(12, 242);
             this.button4.Name = "button4";
@@ -197,10 +206,12 @@
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button0_Click);
+            this.button4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button6
             // 
             this.button6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button6.CausesValidation = false;
             this.button6.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button6.Location = new System.Drawing.Point(170, 242);
             this.button6.Name = "button6";
@@ -209,10 +220,12 @@
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button0_Click);
+            this.button6.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.CausesValidation = false;
             this.button2.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button2.Location = new System.Drawing.Point(91, 304);
             this.button2.Name = "button2";
@@ -221,22 +234,26 @@
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button0_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button3.CausesValidation = false;
             this.button3.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button3.Location = new System.Drawing.Point(170, 304);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(73, 56);
-            this.button3.TabIndex = 3;
+            this.button3.TabIndex = 17;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button0_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.CausesValidation = false;
             this.button1.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button1.Location = new System.Drawing.Point(12, 304);
             this.button1.Name = "button1";
@@ -245,10 +262,12 @@
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button0_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // button0
             // 
             this.button0.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button0.CausesValidation = false;
             this.button0.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button0.Location = new System.Drawing.Point(12, 366);
             this.button0.Name = "button0";
@@ -257,6 +276,7 @@
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = false;
             this.button0.Click += new System.EventHandler(this.button0_Click);
+            this.button0.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
             // 
             // str
             // 
@@ -272,6 +292,20 @@
             this.str.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.str.TextChanged += new System.EventHandler(this.str_TextChanged);
             // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button8.CausesValidation = false;
+            this.button8.Font = new System.Drawing.Font("맑은 고딕", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button8.Location = new System.Drawing.Point(91, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(73, 56);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button0_Click);
+            this.button8.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button0_KeyDown);
+            // 
             // CalculatorV1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -285,8 +319,8 @@
             this.Controls.Add(this.sumBtn);
             this.Controls.Add(this.subBtn);
             this.Controls.Add(this.backspaceBtn);
-            this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.clrBtn);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button5);
@@ -296,8 +330,8 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button0);
-            this.Name = "Endas Calculator";
-            this.Text = "Endas Calculator";
+            this.Name = "CalculatorV1";
+            this.Text = "앤다스 계산기 V1.0";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -313,7 +347,6 @@
         private System.Windows.Forms.Button subBtn;
         private System.Windows.Forms.Button backspaceBtn;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button clrBtn;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button5;
@@ -324,6 +357,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button0;
         private System.Windows.Forms.TextBox str;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- calcV2/CalculatorV1.cs
+++ calcV2/CalculatorV1.cs
@@ -13,12 +13,16 @@ namespace calcV2
 
     public partial class CalculatorV1 : Form
     {
+        // calculator 폼의 모든 요소들(변수들)의 값을 임시로 저장해둘 수 있는 클래스 입니다.
+        // SaveStatus()를 통해 tempData객체에 현재정보를 업데이트하고
+        // LoadStatas()를 통해 tempData에 저장된 정보들을 가져와서 현재의 Form 에 적용시킵니다.
         Calc tempData = new Calc();
 
         double finalResult, result;
         string op, nextOp;
         bool op_pressed, sum_pressed, sub_pressed, enter_clicked;
 
+        // 생성자
         public CalculatorV1()
         {
             InitializeComponent();
@@ -26,37 +30,42 @@ namespace calcV2
             op = nextOp = "";
             op_pressed = sum_pressed = sub_pressed = enter_clicked = false;
         }
-        
 
-        private void button0_Click(object sender, EventArgs e)
+        /// 숫자버튼(0~9) 를 클릭했을때 발생하는 이벤트 *****
+        private void button0_Click(object sender, EventArgs e) 
         {
-            if (calcStr.Text == "0" || op_pressed)
+            if (calcStr.Text == "0" || op_pressed) // 연산자 이후에 숫자를 입력받는 경우.
             {
                 calcStr.Clear();
                 op_pressed = false;
             }
-            if (enter_clicked && !op_pressed) // enter쳐서 계산이 완료된 상태에서 새로운 숫자를 누르면 
-                initializeForm(); // -> 처음부터 새로시작
+            if (enter_clicked && !op_pressed) // enter쳐서 계산이 완료된 시점에서 숫자를 누른다면, 초기화 한 후, 새로운 계산 시작
+                initializeForm();
 
+            // 일반적인 경우.
+            // 두 개의 TextBox에 입력된 값을 넣습니다.
             Button btn = (Button)sender;
             calcStr.Text = calcStr.Text + btn.Text;
             str.Text = str.Text + btn.Text;
             op_pressed = false;
             enter_clicked = false;
         }
-        
-        private void backspaceBtn_Click(object sender, EventArgs e)
+
+        /// backspace 버튼을 클릭했을때 발생하는 이벤트 *****
+        private void backspaceBtn_Click(object sender, EventArgs e) 
         {
-            if (enter_clicked || op_pressed)
+            if (enter_clicked || op_pressed) // 직전에 엔터/연산자를 입력했었다면, 지울것이 없으므로 무시.
                 return;
-            if (double.Parse(calcStr.Text) < 10.0)
+
+            if (double.Parse(calcStr.Text) < 10.0) // 현재 지우려는 값이 10 미만이면, 그냥 0으로 세팅.
             {
                 calcStr.Text = "0";
                 str.Text = str.Text.Remove(str.Text.Length - 1);
                 str.Text += "0";
-
                 return;
             }
+            // 일반적인 경우.
+            // 입력받았던 숫자에서 1의 자리수를 지워줍니다. (/10)
             else
             {
                 str.Text = str.Text.Remove(str.Text.Length - 1);
@@ -64,8 +73,10 @@ namespace calcV2
             }
         }
 
+        /// CE 버튼을 클릭했을때 발생하는 이벤트 *****
         private void clrBtn_Click(object sender, EventArgs e)
         {
+            // 모든값을 초기화 시켜줍니다.
             calcStr.Text = "0";
             str.Text = "";
             result = 0.0;
@@ -74,8 +85,10 @@ namespace calcV2
             op_pressed = sum_pressed = sub_pressed = false;
         }
 
+        /// 엔터 버튼을 클릭했을때 발생하는 이벤트 *****
         private void enterBtn_Click(object sender, EventArgs e)
         {
+            // 엔터를 입력하면 마지막 연산(nextOp에 저장된 연산자)을 수행하고 결과값을 표시합니다.
             switch (nextOp)
             {
                 case "+":
@@ -94,40 +107,46 @@ namespace calcV2
             }
             str.Text = calcStr.Text = finalResult.ToString();
 
-            SaveStatus();
+            SaveStatus(); // 이어서 새로운 연산을 시작할 경우에 대비하여 현재 상태 저장.
             
-            //op_pressed = sum_pressed = sub_pressed = false;
             enter_clicked = true;
-            //result = 0.0;
-            //op = nextOp = "";
         }
 
+        /// 연산자 버튼을 클릭했을때 발생하는 이벤트 *****
         private void operand_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if (enter_clicked) // 엔터를 누른 후 첫 입력이 연산자라면, 
+            if (enter_clicked) // 엔터를 누른 후 첫 입력이 연산자라면, 엔터의 결과로 생성된 결과값을 사용하여 새로운 연산 시작.
             {
                 initializeForm();
                 calcStr.Text = str.Text = tempData.calcStr;
             }
             enter_clicked = false;
 
-            if (op_pressed) // 이미 연산자가 눌린 상태에서 또 연산자를 누르면 연산자만 변경
+            if (op_pressed) // 이미 연산자가 눌린 상태에서 연달아 연산자를 누른 경우라면,  연산자만 변경해야 합니다.
             {
                 str.Text = str.Text.Remove(str.Text.Length - 1);
                 str.Text = str.Text + btn.Text;
-                
-                if (!IsEqualOp(btn.Text, nextOp)) // +- 에서 */ 이것만 문제. 아니라면 if문 패스
+
+                // +,- 에서 *,/ 로, 또는 그 반대로 변하는 경우, 문제가 생기므로 따로 처리합니다.
+                // (+,- -> +,- 또는 *,/ -> *,/ 의 경우는 안전하므로 pass)
+                if (!IsEqualOp(btn.Text, nextOp)) 
                 {
-                    SetStatus(tempData); // 직전에 저장된 데이터를 불러와서 */ 연산으로 새로수행.
+                    LoadStatus(tempData); // 직전에 저장된 데이터를 불러와서 */ 연산으로 새로수행.
                     operand_Click(sender, e);
                 }
                 nextOp = btn.Text;
                 return;
             }
-            SaveStatus();
-            if (nextOp.Equals("")) { // 첫 연산자 클릭이라면, 연산 없이 값 저장 후 리턴.
-                result = (finalResult == 0.0) ? (double.Parse(calcStr.Text)) : 0.0; // = (enter) 기호 클릭 후 이어서 연산 진행하는 경우와 구분
+            
+            SaveStatus(); // 연산자가 후에 바뀔 경우에 대비하여 상태저장.
+
+            // 프로그램 실행 후 첫 연산자 클릭이라면, 연산 없이 변수값 update 후 return
+            if (nextOp.Equals("")) { 
+                if (str.Text.Equals("")) // 숫자입력없이 연산자 누른경우, 0 + 와 같은 식으로 0을 대상으로 연산을 진행합니다.
+                    TextBoxAppend(ref str, "0");
+
+                result = (finalResult == 0.0) ? (double.Parse(calcStr.Text)) : 0.0; // = (enter) 기호 클릭 후 이어서 연산 진행하는 경우와 구분하기 위함
                 if (btn.Text.Equals("+") || btn.Text.Equals("-")) {
                     if(!enter_clicked) 
                         Calculate(ref finalResult, "+", result);
@@ -142,18 +161,15 @@ namespace calcV2
             op_pressed = true;
             op = nextOp;
             nextOp = btn.Text;
-
-            if (str.Text.Equals("")) // 숫자 입력없이 바로 연산자만 누르면 0 + 이런식으로 앞에 0 추가해준다. 0 + ..
-                TextBoxAppend(ref str, "0");
             str.Text = str.Text + btn.Text;
 
-            
+            // 예외 처리 끝. 실제 연산 수행
             switch (nextOp)
             {
                 case "+":
                 case "-":
-                    //if(직전 연산자가 */ 이면 마저 수행하고 result더해줘야해) 현재 입력받은 키가 +-이면 finalResult업데이트후 result초기화
-                    //else 직전에도 +-였으면 
+                    //if(직전 연산자가 */ 이면) 마저 수행하고 result더해줍니다. 
+                    //else 직전에도 +-였으면, 단순히 현재 입력값을 더하거나 빼줍니다.
 
                     if (op.Equals("+") || op.Equals("-"))
                         Calculate(ref finalResult, op, double.Parse(calcStr.Text));
@@ -187,16 +203,17 @@ namespace calcV2
 
                 default:
                     MessageBox.Show("보이면 에러");
-                    //finalResult = result;
-                    //result = 0.0;
                     break;
             }
         }
+        
+        /// TextBox의 Text에 값을 간편하게 넣기 위한 함수. 중요하지 않음
         private void TextBoxAppend(ref System.Windows.Forms.TextBox box, string orgin)
         {
             box.Text = box.Text + orgin;
         }
 
+        /// 넘겨받은 연산자에 따라 그에 맞는 연산을 알아서 해주는 function
         private void Calculate(ref double reference, string op, double value)
         {
             switch (op)
@@ -218,6 +235,8 @@ namespace calcV2
                     break;
             }
         }
+
+        /// 연산자 비교를 간단히 하기 위해만든 function
         private bool IsEqualOp(string a, string b)
         {
             if (a.Equals("+") || a.Equals("-"))
@@ -235,6 +254,8 @@ namespace calcV2
                     return false;
             }
         }
+        
+        /// tempData객체에 현재 상태를 store합니다.
         private void SaveStatus()
         {
             tempData.finalResult = finalResult;
@@ -248,7 +269,9 @@ namespace calcV2
             tempData.calcStr = calcStr.Text;
             tempData.str = str.Text;
         }
-        private void SetStatus(Calc c)
+
+        /// tempData객체의 정보를 load합니다.
+        private void LoadStatus(Calc c)
         {
             finalResult = c.finalResult;
             result = c.result;
@@ -261,6 +284,8 @@ namespace calcV2
             calcStr.Text = c.calcStr;
             str.Text = c.str;
         }
+
+        /// form을 초기화 하는 function
         private void initializeForm()
         {
             finalResult = result = 0.0;
@@ -269,6 +294,74 @@ namespace calcV2
             str.Text =  "";
             calcStr.Text = "";
         }
+
         private void str_TextChanged(object sender, EventArgs e) { return; }
+
+        ///  keyboard input을 처리하기 위한 함수입니다. key press event가 발생하면 해당하는 키의 click event handler와 연결시켜줍니다.
+        private void button0_KeyDown(object sender, KeyEventArgs e)
+        {
+            string input = e.KeyData.ToString();
+            Button btn = new Button();
+            object obj;
+            switch (input)
+            {
+                case "D1":
+                case "NumPad1":
+                case "D2":
+                case "NumPad2":
+                case "D3":
+                case "NumPad3":
+                case "D4":
+                case "NumPad4":
+                case "D5":
+                case "NumPad5":
+                case "D6":
+                case "NumPad6":
+                case "D7":
+                case "NumPad7":
+                case "D8":
+                case "NumPad8":
+                case "D9":
+                case "NumPad9":
+                case "D0":
+                case "NumPad0":
+                    btn.Text = input.Last().ToString();
+                    obj = (object)btn;
+                    button0_Click(obj, null);
+                    break;
+                case "Multiply":
+                    btn.Text = "*";
+                    obj = (object)btn;
+                    operand_Click(obj, null);
+                    break;
+                case "Divide":
+                    btn.Text = "/";
+                    obj = (object)btn;
+                    operand_Click(obj, null);
+                    break;
+                case "Add":
+                    btn.Text = "+";
+                    obj = (object)btn;
+                    operand_Click(obj, null);
+                    break;
+                case "Subtract":
+                    btn.Text = "-";
+                    obj = (object)btn;
+                    operand_Click(obj, null);
+                    break;
+                case "Enter":
+                    enterBtn_Click(null, null);
+                    break;
+                case "Back":
+                    backspaceBtn_Click(null, null);
+                    break;
+                case "Delete":
+                    clrBtn_Click(null, null);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
     }
 }
